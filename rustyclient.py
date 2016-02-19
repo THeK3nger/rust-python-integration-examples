a767230ee7c86341868dfcbac20d69bed5beaad8
Davide Aversa <thek3nger@gmail.com>
2016-02-19 10:49:14 +0100

    Libc Types for Parameters

--- rustyclient.py
+++ rustyclient.py
@@ -3,6 +3,10 @@ import ctypes
 lib = ctypes.WinDLL(".\\target\\release\\rustypython.dll")
 
 #lib = cdll.LoadLib(".\target\release\rustypython.dll")
+print("Running process()")
 lib.process()
+
+print("Running process2(5)")
+lib.process2(5)
 
 print("done")
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,29 @@
+extern crate libc;
+
+use libc::{size_t,int32_t};
 use std::thread;
 
 #[no_mangle]
 pub extern fn process() {
     let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in 0..5_000_000 {
+                x += 1
+            }
+            x
+        })
+    }).collect();
+
+    for h in handles {
+        println!("Thread finished with count={}",
+        h.join().map_err(|_| "Could not join a thread!").unwrap());
+    }
+}
+
+#[no_mangle]
+pub extern fn process2(threads_number: int32_t) {
+    let handles: Vec<_> = (0..threads_number).map(|_| {
         thread::spawn(|| {
             let mut x = 0;
             for _ in 0..5_000_000 {
