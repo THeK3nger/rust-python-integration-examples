a5c2df42267de8507dac697f20b4e244510634d7
Davide Aversa <thek3nger@gmail.com>
2016-02-20 19:06:14 +0100

    Tons of comments in Rust and Python code.

--- rustyclient.py
+++ rustyclient.py
@@ -1,12 +1,31 @@
+'''
+This is a simple client script who will run the Rust binary library.
+
+@author: Davide Aversa <thek3nger@gmail.com>
+
+NOTE: Before using this script you neet to compile the rust library. Do that
+with the command
+
+    cargo build --release
+    
+This script is for Windows. Linux version will come.
+'''
+
+# Import the stadard interface library between Python and C libraries.
 import ctypes
 
+# We load the dll using WinDLL (windows format). For Linux try to comment
+# this line and then uncomment the next one. 
 lib = ctypes.WinDLL(".\\target\\release\\rustypython.dll")
-
 #lib = cdll.LoadLib(".\target\release\rustypython.dll")
+
+# We execute the Rust process() function we defined in src/lib.rs
 print("Running process()")
 lib.process()
 
+# We execute the Rust process2() function we defined in src/lib.rs
 print("Running process2(5)")
 lib.process2(5)
 
+# Congratulations!
 print("done")
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
+//! EMBEDDING RUST IN PYTHON
+//! Examples on how to write Rust libraries callable by a Python script.
+//!
+//! Author: Davide Aversa <thek3nger@gmail.com>
+//! ---------------------------------------------------------------------------
+
+
+/// Libc provides several C-friendly data types. This is used to write 
+/// functions callable by an external languages **or** to call external
+///C libraries.  
 extern crate libc;
 
+/// Use this to load in the namespace the `size_t` and `int32_t` types.
 use libc::{size_t,int32_t};
+
+/// Use this to load the standard library interface to threading.
 use std::thread;
 
+/// The first function we want to try is a simple no-input/no-outut function.
+///
+/// The algorithm spawns 10 threads, and each one will increas a counter
+/// 5 milion times in order to simulate some work. At the end the function
+/// waits for the thread completitions and retun nothing. 
 #[no_mangle]
 pub extern fn process() {
     let handles: Vec<_> = (0..10).map(|_| {
@@ -21,6 +39,12 @@ pub extern fn process() {
     }
 }
 
+/// This is the next logical step, write a function who receive an integral
+/// input and return nothing.
+///
+/// This time the algorithm spawns the number of threads passed as an argument.
+/// Note that the function does not take a generic Rust i32 **but** the libc
+/// defined type `int32_t`; the equivalent in the C interface for i32.
 #[no_mangle]
 pub extern fn process2(threads_number: int32_t) {
     let handles: Vec<_> = (0..threads_number).map(|_| {
